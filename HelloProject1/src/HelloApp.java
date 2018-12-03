61b1f78abf05bc1bff1730aeae3c02587d297d4d
Dylaloon <35204392+Dylaloon@users.noreply.github.com>
2018-12-03 14:32:39 -0400

    Update HelloApp.java

--- HelloProject1/src/HelloApp.java
+++ HelloProject1/src/HelloApp.java
@@ -3,7 +3,7 @@ public class HelloApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("Hello Dylaloon");
+			System.out.println("Hello Dylan Lohnes");
 	}
 
 }
